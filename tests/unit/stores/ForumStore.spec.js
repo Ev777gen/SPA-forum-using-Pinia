d1779112161fe9f6e8879bf87b6a841e09b2fad0
Ev777gen <fortheartist@mail.ru>
2023-05-15 17:53:58 +0500

    add empty ForumStore test

---
+++ tests/unit/stores/ForumStore.spec.js
@@ -0,0 +1,12 @@
+import { setActivePinia, createPinia } from 'pinia';
+import { useForumStore } from '@/stores/ForumStore';
+
+describe('ForumStore', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it('should ...', () => {
+    
+  });
+});
